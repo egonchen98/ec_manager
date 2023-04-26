7e3de622f6106257e8a6534f40eee60bf30cf212
egon <xwsever@outlook.com>
2023-04-26 14:47:40 +0800

    'v1'

--- request_data.py
+++ request_data.py
@@ -24,7 +24,7 @@ def get_req(req_dict: dict):
     expver=1,
         date=$date,
         levtype=sfc,
-        $number_affix number=1/to/50, 
+        $number_affix number=$number,
         param=$param,
         step=$step,
         stream=enfo,
@@ -32,23 +32,24 @@ def get_req(req_dict: dict):
         type=$type,
         area=$area,
         grid=0.25/0.25,
-        target='output1.grib'
     """)
 
-    req1 = """
-    retrieve,
+    req2 = """
+    list,
     class=od,
-    date=2022-06-01,
+    date=2017-01-01/to/2017-12-31,
     expver=1,
     levtype=sfc,
-    number=1/to/50,
+    number=1/to/10,
     param=121/122,
     step=0/to/360/by/6,
     stream=enfo,
     time=00:00:00,
     type=pf,
     area=31.375/121/31.25/121.125,
-    GRID=0.25/0.25
+    GRID=0.25/0.25,
+    output=cost,
+    target='output'
     """
 
     req1 = req.substitute(req_dict)
@@ -70,16 +71,16 @@ class ReqManager:
             target_file = f'{self.data_dir}\\{file_id}'
             if target_file in os.listdir(self.data_dir):
                 continue
-            req = get_req(row)
-            pool.submit(send_request, req, target_file)
-            time.sleep(2*60)
+            req = get_req(row.to_dict())
+            print(req)
+            # pool.submit(send_request, req, target_file)
+            # time.sleep(2*60)
 
         pool.shutdown()
 
 
 def main():
     """Main function"""
-    # send_request(get_req())
 
     config = json.loads(Path('./resources/config.json').read_text())
     manager = ReqManager(data_dir=config['database'])
--- resources/tools.py
+++ resources/tools.py
@@ -4,8 +4,7 @@ import pandas as pd
 
 def generate_enfo_req():
     """Generate requests string to send request"""
-    dates = [f'{year}-{month:02}-01/to/{year}-{month:02}-{monthrange(year, month)[1]}' for year in range(2017, 2023) for month in range(1, 13)]  # get all date to be looped
-    dates = [f'{year}-{month:02}-01/to/{year}-{month:02}-03' for year in range(2017, 2023) for month in range(1, 13)]  # TODO: detelete this
+    dates = [f'{year}-01-01/to/{year}-12-31' for year in range(2017, 2023)]  # get all date to be looped
     param_steps = [['121/122', '0/to/360/by/6'], ['151/165/166/168/169/175/176/177/228', '0/to/360/by/24']]
     area = '31.375/121/31.25/121.125'  # Kunshan
 
@@ -13,12 +12,16 @@ def generate_enfo_req():
     for date in dates:
         for param_step in param_steps:
             # [file_id, type, date, param, step, is_num]
-            req1 = [f'cf_{date[:10]}_tem.grib', 'cf', '#', date, area] + param_step
-            req2 = [f'pf_{date[:10]}_nontem.grib', 'pf', '', date, area] + param_step
-            all_req_kw.append(req1)
-            all_req_kw.append(req2)
+            pf_numbers = ['1/to/10', '11/to/20', '21/to/30', '31/to/40', '41/to/50']
+            req_cf = [f'cf_{date[:10]}_0.grib', 'cf', '#', date, area] + param_step + ['None']
+            req_pfs = []
+            for i in range(1, 1+len(pf_numbers)):
+                req_pfs.append([f'pf_{date[:10]}_{i}.grib', 'pf', '', date, area] + param_step + [pf_numbers[i-1]])
 
-    req_df = pd.DataFrame(all_req_kw, columns=['file_id', 'type', 'number_affix', 'date', 'area', 'param', 'step'])
+            all_req_kw.extend(req_pfs)
+            all_req_kw.append(req_cf)
+
+    req_df = pd.DataFrame(all_req_kw, columns=['file_id', 'type', 'number_affix', 'date', 'area', 'param', 'step', 'number'])
     req_df['request_time'] = None
 
     req_df.to_csv('job_manager.csv', index=False)
